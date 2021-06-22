69c4832922f30421ea5b23a8d1b9c94f8a6c5415
thiago <thiagosilva128@gmail.com>
2021-06-22 10:23:37 -0300

    test subscription

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,4 @@
-import { signIn, useSession } from "next-auth/client"
+import { signIn, getSession } from "next-auth/client"
 import { useRouter } from "next/router"
 import { api } from "../../services/api"
 import { getStripeJs } from "../../services/stripe-js"
@@ -9,30 +9,32 @@ import styles from "./styles.module.scss"
 // }
 
 export function SubscribeButton() {
-    const [session] = useSession()
     const router = useRouter()
+   
+    async function handleSubscribe() {
+        const session = await getSession()
 
-    const { activeSubscription } = session
-
-     async function handleSubscribe() {
+        console.log(session)
         if(!session) {
             signIn('github')
             return
         }
 
-        if(activeSubscription) {
+        if(!session?.activeSubscription) {
+           
+            try{
+                const response = await api.post('/subscribe')
+                const { sessionId } = response.data
+    
+                const stripe = await getStripeJs()
+                await stripe.redirectToCheckout({ sessionId })
+            
+            } catch(err){
+                alert(err.message)
+            }
+            return
+        } else {
             router.push('/posts')
-        }
-
-        try{
-            const response = await api.post('/subscribe')
-            const { sessionId } = response.data
-
-            const stripe = await getStripeJs()
-            await stripe.redirectToCheckout({ sessionId })
-        
-        } catch(err){
-            alert(err.message)
         }
 
     }
